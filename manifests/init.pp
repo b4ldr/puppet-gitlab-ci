bb64e1e2cc6bea30d3ead091afc9a6d482835d60
Lee Boynton <lee@lboynton.com>
2013-01-22 16:33:25 +0000

    Add redis

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,16 @@
 # Install gitlab-ci
 class gitlab_ci {
+    if $::osfamily == 'RedHat' and $::operatingsystem != 'Fedora' {
+        include epel
+    }
+
     user { 'gitlab_ci':
         ensure  => present,
         comment => 'GitLab CI',
         system  => true,
+    }
+
+    package { 'redis':
+        ensure  => installed,
     }
 }
